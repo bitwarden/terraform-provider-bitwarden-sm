f9cb0f530b6218d99bab7017d9c3f929fee30b1b
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-07-31 21:18:52 +0000

    we can dynamically link using gcc now

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install -y musl-tools
-
 git config --global --add safe.directory "$PWD"
 
-# configure Go to use musl-gcc
-go env -w CC=musl-gcc
-
 # create ~/.terraformrc dev override
-cat <<EOF > ~/.terraformrc
+cat <<EOF >~/.terraformrc
 provider_installation {
   dev_overrides {
       "registry.terraform.io/bitwarden/bitwarden-sm" = "/go/bin"
@@ -21,5 +15,5 @@ echo "
 devcontainer setup complete!
 
 To build and install the terraform provider, run:
-  go install -ldflags '-linkmode external -extldflags \"-static -Wl,-unresolved-symbols=ignore-all\"' .
+  go install -ldflags '-linkmode external -extldflags \"-lm\"' .
 "
