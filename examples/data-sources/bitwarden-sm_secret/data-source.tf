507ba664cfb7fa28a5c69621d332e8c3abe06ba3
Adam Thomas Jagger Bramley <atjbramley@hotmail.com>
2025-07-17 15:54:26 +0200

    update data-source.tf to refer to data objects

--- examples/data-sources/bitwarden-sm_secret/data-source.tf
+++ examples/data-sources/bitwarden-sm_secret/data-source.tf
@@ -8,10 +8,10 @@ output "secret" {
     key = data.bitwarden-sm_secret.secret.key
     # The actual secret value is marked sensitive and will not be printed to stdout
     # value          = data.bitwarden-sm_secret.secret.value
-    note            = resource.bitwarden-sm_secret.secret.note
-    project_id      = resource.bitwarden-sm_secret.secret.project_id
-    organization_id = resource.bitwarden-sm_secret.secret.organization_id
-    creation_date   = resource.bitwarden-sm_secret.secret.creation_date
-    revision_date   = resource.bitwarden-sm_secret.secret.revision_date
+    note            = data.bitwarden-sm_secret.secret.note
+    project_id      = data.bitwarden-sm_secret.secret.project_id
+    organization_id = data.bitwarden-sm_secret.secret.organization_id
+    creation_date   = data.bitwarden-sm_secret.secret.creation_date
+    revision_date   = data.bitwarden-sm_secret.secret.revision_date
   }
 }
